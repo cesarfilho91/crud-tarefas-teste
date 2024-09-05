5482a9a0183d466a9439e7f3501cb7d153c7e18e
cesarfilho91 <31566550+cesarfilho91@users.noreply.github.com>
2024-09-05 04:07:03 -0300

    server

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ if (!mongoURI) {
 }
 
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true
 })
 .then(() => console.log('Conectado ao MongoDB'))
 .catch(err => console.error('Erro ao conectar ao MongoDB', err));
