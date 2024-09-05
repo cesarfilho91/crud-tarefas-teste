7f6fe9292098217ffd1fd5a859315e70aec0e537
cesarfilho91 <31566550+cesarfilho91@users.noreply.github.com>
2024-09-05 00:33:23 -0300

    Ajuste cors

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,19 @@ const cors = require('cors');
 const app = express();
 const taskRoutes = require('./routes/task');
 
-require('dotenv').config();
-
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: 'https://crud-tarefas-teste.vercel.app',
 }));
 
-app.use(express.json()); 
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Conectado ao MongoDB'))
-.catch(err => console.error('Erro ao conectar ao MongoDB', err));
+app.use(express.json());
 
 app.use('/api/tarefas', taskRoutes);
 
-app.listen(5000, () => {
-  console.log('Servidor rodando na porta 5000');
-});
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(error => console.error('Erro ao conectar ao MongoDB:', error));
