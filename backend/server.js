d1b118ba671862a3e822ad0609d47cc3024ccf55
cesarfilho91 <31566550+cesarfilho91@users.noreply.github.com>
2024-09-05 00:17:14 -0300

    Mongo

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,22 @@ const cors = require('cors');
 const app = express();
 const taskRoutes = require('./routes/task');
 
-//app.use(cors());
+require('dotenv').config();
+
 app.use(cors({
-  origin: 'https://crud-tarefas-teste-2wua0yhzn.vercel.app',
+  origin: process.env.CORS_ORIGIN,
 }));
 
 app.use(express.json()); 
 
-app.use('/api/tarefas', taskRoutes);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Conectado ao MongoDB'))
+.catch(err => console.error('Erro ao conectar ao MongoDB', err));
 
-mongoose.connect('mongodb://localhost/redizz_db', { });
+app.use('/api/tarefas', taskRoutes);
 
 app.listen(5000, () => {
   console.log('Servidor rodando na porta 5000');
