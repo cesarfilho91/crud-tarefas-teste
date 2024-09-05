5d11f416f3bf447aa4a7ad0371f49d42543ac463
cesarfilho91 <31566550+cesarfilho91@users.noreply.github.com>
2024-09-05 02:08:47 -0300

    mongo url

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,13 @@ app.use(express.json());
 
 app.use('/api/tarefas', taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('Conectado ao MongoDB'))
 .catch(err => console.error('Erro ao conectar ao MongoDB', err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000');
 });
