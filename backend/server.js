ed4f16a4ce6938c27fd068df39775b3212d3c81f
cesarfilho91 <31566550+cesarfilho91@users.noreply.github.com>
2024-09-05 02:59:25 -0300

    ajuste server

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,10 @@ app.use(cors({
 app.use(express.json());
 
 const mongoURI = 'mongodb://mongo:NUwGKyDrZguPCEQwzjcgZffFGtDnvOqK@autorack.proxy.rlwy.net:44944';
-if (!mongoURI) {
-  console.error('A variável de ambiente MONGO_URL não está definida.');
-  process.exit(1);
-}
 
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true
 })
 .then(() => console.log('Conectado ao MongoDB'))
 .catch(err => console.error('Erro ao conectar ao MongoDB', err));
